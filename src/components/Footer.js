17ce03cc561503d5564056756b257be9167bb1a6
Yusuf Isbilir <dev.yusufisbilir@gmail.com>
2021-11-24 17:06:41 +0300

    Complete Footer section.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,133 @@
+import Menu from "./ui/Menu";
+import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
+import { BiGlobe } from "react-icons/bi";
+
 const Footer = () => {
-  return <div>footer</div>;
+  const menus = [
+    {
+      title: "Getir'i Keşfedin",
+      items: [
+        {
+          title: "Hakkımızda",
+        },
+        {
+          title: "Kariyer",
+        },
+        {
+          title: "İletişim",
+        },
+        {
+          title: "COVID-19 Duyuru",
+        },
+        {
+          title: "Sosyal Sorumluluk Projesi",
+        },
+      ],
+    },
+    {
+      title: "Yardıma mı ihtiyacınız var?",
+      items: [
+        {
+          title: "Sıkça Sorulan Sorular",
+        },
+        {
+          title: "Kişisel Verilerin Korunması",
+        },
+        {
+          title: "Gizlilik Politikası ",
+        },
+        {
+          title: "Kullanım Koşulları",
+        },
+        {
+          title: "Çerez Politikası",
+        },
+      ],
+    },
+    {
+      title: "İş Ortağımız Olun",
+      items: [
+        {
+          title: "Bayimiz Olun",
+        },
+        {
+          title: "Deponuzu Kiralayın",
+        },
+        {
+          title: "GetirYemek Restoranı Olun",
+        },
+        {
+          title: "GetirÇarşı İşletmesi Olun",
+        },
+      ],
+    },
+  ];
+
+  return (
+    <div className="bg-white mt-10">
+      <div className="container mx-auto">
+        <div className="grid grid-cols-4  pt-10">
+          <section>
+            <nav className="grid gap-y-4">
+              <h6 className="text-lg text-primary-brand-color">
+                Getir'i indirin!
+              </h6>
+              <img
+                src="https://getir.com/_next/static/images/appstore-tr-141ed939fceebdcee96af608fa293b31.svg"
+                alt=""
+              />
+              <img
+                src="https://getir.com/_next/static/images/googleplay-tr-6b0c941b7d1a65d781fb4b644498be75.svg"
+                alt=""
+              />
+              <img
+                src="https://getir.com/_next/static/images/huawei-appgallery-tr-4b890fa3167bc62f9069edaf45aa7f30.svg"
+                alt=""
+              />
+            </nav>
+          </section>
+          {menus.map((menu, index) => (
+            <Menu key={index} {...menu} />
+          ))}
+        </div>
+        <div className="flex justify-between items-center border-t border-gray-200 mt-6 py-6">
+          <div className="text-xs text-gray-700 flex gap-x-6">
+            &copy; 2021 Getir
+            <a href="#" className="text-primary-brand-color">
+              Bilgi Toplumu Hizmetleri
+            </a>
+          </div>
+          <nav className="flex gap-x-3">
+            <a
+              href="#"
+              className="w-8 h-8 rounded-lg text-gray-500 transition-colors hover:bg-primary-brand-color hover:bg-opacity-10 flex items-center justify-center hover:text-primary-brand-color"
+            >
+              <FaFacebook size={20} />
+            </a>
+            <a
+              href="#"
+              className="w-8 h-8 rounded-lg text-gray-500 transition-colors hover:bg-primary-brand-color hover:bg-opacity-10 flex items-center justify-center hover:text-primary-brand-color"
+            >
+              <FaTwitter size={20} />
+            </a>
+            <a
+              href="#"
+              className="w-8 h-8 rounded-lg text-gray-500 transition-colors hover:bg-primary-brand-color hover:bg-opacity-10 flex items-center justify-center hover:text-primary-brand-color"
+            >
+              <FaInstagram size={20} />
+            </a>
+            <a
+              href="#"
+              className="text-gray-500 h-8 flex items-center px-2 border text-sm  gap-x-2 border-gray-200 rounded-lg"
+            >
+              <BiGlobe size={20} />
+              Türkçe (TR)
+            </a>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
---
+++ src/components/ui/Menu.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Menu = ({ title, items }) => {
+  return (
+    <section>
+      <nav className="grid gap-y-4">
+        <h6 className="text-lg text-primary-brand-color">{title}</h6>
+        <nav>
+          <ul className="grid gap-y-2">
+            {items.map((item, key) => (
+              <li key={key}>
+                <a href="#">{item.title}</a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </nav>
+    </section>
+  );
+};
+
+export default Menu;
