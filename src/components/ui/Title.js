520644afd90ebba7add2d960cdcbeec5844dc033
Yusuf Isbilir <dev.yusufisbilir@gmail.com>
2021-11-24 19:24:05 +0300

    Make responsive title ui section.

--- src/components/ui/Title.js
+++ src/components/ui/Title.js
@@ -1,5 +1,7 @@
 const Title = ({ children }) => {
-  return <h3 className="text-sm font-semibold mb-3">{children}</h3>;
+  return (
+    <h3 className="text-sm font-semibold mb-3 px-6 md:px-0">{children}</h3>
+  );
 };
 
 export default Title;
