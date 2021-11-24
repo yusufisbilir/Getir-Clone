ab55e8528c6ea5f1a7b7ece3e0342c0906a93221
Yusuf Isbilir <dev.yusufisbilir@gmail.com>
2021-11-24 17:34:17 +0300

    Make responsive campaigns section.

--- src/components/Campaigns.js
+++ src/components/Campaigns.js
@@ -24,9 +24,29 @@ const Campaigns = () => {
     dots: false,
     infinite: true,
     speed: 500,
-    slidesToShow: 3,
+    slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   };
   return (
     <div className="container mx-auto py-8">
